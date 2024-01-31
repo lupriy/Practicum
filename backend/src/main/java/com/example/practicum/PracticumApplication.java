0053d40eb43c6b85fbe27bd61d35aac84227fb63
lupriy <artemsved66@gmail.com>
2024-01-31 21:43:28 +0500

    add back

---
+++ backend/src/main/java/com/example/practicum/PracticumApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticumApplication.class, args);
+    }
+
+}
