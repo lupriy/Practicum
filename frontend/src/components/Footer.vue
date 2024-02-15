58691f33c576182cfdc09326a31a9b901248d16a
lupriy <artemsved66@gmail.com>
2024-02-15 19:29:53 +0500

    component header and footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,34 +1,35 @@
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
+
 }
 </script>
 
 <template>
-  <footer class="mt-10 md:mt-28 py-4">
-    <div class="flex flex-col md:flex-row justify-between items-center">
-      <div class="ml-14">
-        <img src="/log.svg" alt="logo">
-      </div>
-      <nav class="text-center text-white md:text-left">
-        <ul class="flex flex-wrap justify-center md:justify-start space-x-4 md:space-x-28 mr-0 md:mr-36">
-          <li class="footerS">О НАС</li>
-          <li class="footerS">ЗАВТРАКИ</li>
-          <li class="footerS">ТРЕНИНГИ</li>
-          <li class="footerS">КУРСЫ</li>
+  <footer class="mt-auto">
+    <div class="p-4 md:py-8">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <a class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <img src="/log.svg"/>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0 ">
+          <li>
+            <a href="#" class="footerS me-4 md:me-6">О НАС</a>
+          </li>
+          <li>
+            <a href="#" class="footerS me-4 md:me-6">ЗАВТРАКИ</a>
+          </li>
+          <li>
+            <a href="#" class="footerS me-4 md:me-6">ТРЕНИНГИ</a>
+          </li>
+          <li>
+            <a href="#" class="footerS">КУРСЫ</a>
+          </li>
         </ul>
-      </nav>
-      <div class="hidden md:flex space-x-2">
-        <div class="w-2 h-2 rounded-full bg-black"></div>
-        <div class="w-2 h-2 rounded-full bg-black"></div>
-        <div class="w-2 h-2 rounded-full bg-black"></div>
-        <div class="w-2 h-2 rounded-full bg-black"></div>
       </div>
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-white">© 2024 Все права защищены.</span>
     </div>
-    <div class="relative bg-black text-white pt-10 md:pt-28">
-      <div class="absolute inset-x-0 bottom-0 h-0.5" style="background-color: #1a202c; width: 85%; left: 7%;"></div>
-    </div>
-    <div class="mt-6 flex text-white justify-center items-center">©2024</div>
   </footer>
 </template>
 
